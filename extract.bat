3ba3ae620cc436e1e0ca25bccab9525479e7a1b4
IDeserveToBeWrited <dodo.coowbooy@gmail.com>
2026-05-06 13:11:34 +0200

    Add locale

--- extract.bat
+++ extract.bat
@@ -1,39 +1,32 @@
 @echo off
-SETLOCAL ENABLEDELAYEDEXPANSION
 
 :: Location for extraction
 SET LOCATION=%userprofile%\Documents\defETS
 
-:: Counters
-SET /A COUNT=0
-SET /A CURRENT=1
+:: Create location
+if not exist %LOCATION% MKDIR %LOCATION%
 
 :: Clean any remaining files
 echo Cleaning the directory
 del /f/s/q %LOCATION% > nul
 rmdir /s/q %LOCATION%
 
-:: Create location
-if not exist %LOCATION% MKDIR %LOCATION%
-
-:: Location to SCS Extractor
-SET EXTRACTOR="C:\Program Files (x86)\Steam\steamapps\common\Euro Truck Simulator 2\scs_extractor.exe"
-:: Get the SCS Game Archive Extractor here: https://modding.scssoft.com/wiki/Documentation/Tools/Game_Archive_Extractor
+:: Location to sk-zk extractor
+SET EXTRACTOR="extractor.exe"
+:: Get the sk-zk extractor here: https://github.com/sk-zk/Extractor/releases
 
 :: Set 7z.exe location for archiving
 SET ZIP="C:\Program Files\7-Zip\7z.exe"
 
-:: Count Archives
-for %%l in (*.scs) DO (IF NOT %%l == base.scs IF NOT %%l == base_cfg.scs IF NOT %%l == base_map.scs IF NOT %%l == base_share.scs IF NOT %%l == base_vehicle.scs IF NOT %%l == core.scs IF NOT %%l == effect.scs IF NOT %%l == locale.scs SET /A COUNT=COUNT+1 )
-
-:: Extract and clean archives
-for %%a in (*.scs) DO (IF NOT %%a == base.scs IF NOT %%a == base_cfg.scs IF NOT %%a == base_map.scs IF NOT %%a == base_share.scs IF NOT %%a == base_vehicle.scs IF NOT %%a == core.scs IF NOT %%a == effect.scs IF NOT %%a == locale.scs echo|set /p="^(!CURRENT!\%COUNT%^) Unpacking %%~na" & %EXTRACTOR% %%a %LOCATION%\%%~na >NUL & echo|set /p=", Clean-up" & (for /D %%c in (%LOCATION%\%%~na\*) do ((IF NOT %%~nc == def RMDIR "%%c" /s /q >nul))) & DEL %LOCATION%\%%~na\%%~na.manifest.sii >nul & echo . Done. & SET /A CURRENT+=1 )
+:: Extract archives
+%EXTRACTOR% -p=/def -S -a -q "C:\Program Files (x86)\Steam\steamapps\common\Euro Truck Simulator 2" -d %LOCATION%
+%EXTRACTOR% -S -q "C:\Program Files (x86)\Steam\steamapps\common\Euro Truck Simulator 2\version.scs" -d %LOCATION% 
+:: Extract locales
+%EXTRACTOR% --deep -S -q "C:\Program Files (x86)\Steam\steamapps\common\Euro Truck Simulator 2\locale.scs" -d %LOCATION%
 
 ECHO Zipping archive
 %ZIP% a -mx=9 %LOCATION%\def %LOCATION%\*
 ECHO Zipped
 
-ECHO Copying to GitHub
-REM rsync -q -rtvua --delete --exclude={".git",".gitattributes",".gitignore","*.md","*.sha*","*.sfv","extract*.bat","scs_extractor.exe"} /cygdrive/C/Users/doode/Documents/defETS /cygdrive/C/Users/doode/Documents/GitHub
-ECHO DONE LOL
+ECHO DONE
 pause
